10ad8f0357fd23ba4d25fbb34097734608e783f1
aymenbraham <aymenbraham71@gmail.com>
2025-02-22 12:11:32 +0100

    Initial Commit

---
+++ InstagramStories/InstagramStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoriesApp.swift
+//  InstagramStories
+//
+//  Created by aymen braham on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramStoriesTests/InstagramStoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoriesTests.swift
+//  InstagramStoriesTests
+//
+//  Created by aymen braham on 22/02/2025.
+//
+
+import Testing
+@testable import InstagramStories
+
+struct InstagramStoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
